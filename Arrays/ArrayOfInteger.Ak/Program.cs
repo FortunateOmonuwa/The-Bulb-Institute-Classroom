b65967fca1909b1e88a64c4d1f827db552ae930c
Munirudeen Akanbi <adsdesigntech@gmail.com>
2024-05-07 22:14:04 +0100

    Arrays of integer done

---
+++ Arrays/ArrayOfInteger.Ak/Program.cs
@@ -0,0 +1,25 @@
+﻿using ArrayOfInteger.Ak.models;
+using System;
+
+namespace ArrayOfInteger.Ak
+{
+    public class Program
+    { 
+        static void Main(string[] args)
+        {
+            //int[] ArrayOfInt = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 };
+
+            var program = new utilities();
+
+            Console.WriteLine("--------------Even number Details--------------------");
+            program.GetEvenDetails();
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("-------------------------------------------------");
+
+
+            Console.WriteLine("--------------Odd number Details--------------------");
+            program.GetOddDetails();
+
+        }
+    }
+}
---
+++ Arrays/ArrayOfInteger.Ak/models/utilities.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOfInteger.Ak.models
+{
+    public class utilities
+    {
+        int[] ArrOfInt = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,13,17,19,22,33,53};
+
+        int sum = 0;
+        int evenCounter;
+        int oddCounter;
+        public void GetEvenDetails()
+        {
+            foreach (int num in ArrOfInt)
+            {
+                if(num % 2 == 0)
+                {
+                    sum += num;
+                    evenCounter++;
+                }
+            }
+                    Console.WriteLine($"The total sum of Even numbers is {sum}\nLength of Even Number {evenCounter}" );
+
+        }
+
+        public void GetOddDetails()
+        {
+            foreach (int num in ArrOfInt)
+            {
+                if (num % 2 == 1)
+                {
+                    sum += num;
+                    oddCounter++;
+                }
+            }
+            Console.WriteLine($"The total sum of Odd numbers is {sum}\nLength of Odd Number {oddCounter}");
+
+        }
+    }
+}
