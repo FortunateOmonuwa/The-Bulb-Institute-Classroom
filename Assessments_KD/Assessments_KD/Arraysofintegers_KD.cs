8f799999eb492150a4de2107ad6020185b9339e0
Kazeem <97337763+thedoskit@users.noreply.github.com>
2024-05-08 16:10:49 +0100

    Update Arraysofintegers_KD.cs

--- Assessments_KD/Assessments_KD/Arraysofintegers_KD.cs
+++ Assessments_KD/Assessments_KD/Arraysofintegers_KD.cs
@@ -11,14 +11,46 @@ namespace Assessments_KD
 
         public static void Main() { 
         int[] arrayOfIntegers = { 3, 4, 5, 9, 3, 24, 2, 1, 32, 12 };
-            int a = 0;
+
+            int isPrime;
+            int isEven;
+            int isOdd;
+            int sumPrime;
+            int sumEven;
+            int sumOdd;
+
+
+        foreach (int num in arrayOfIntegers) 
+            {
+                if (arrayOfIntegers[num] % num == 0) 
+                {
+                    isPrime =2;
+                    for (int i = 2; i <= Math.Sqrt(i); i++)
+                    {
+
+                        isPrime++;
+
+                        Console.WriteLine(num);
+                    }
+                }
+
+
+                /**else if (arrayOfIntegers[num] % 2 == 0) 
+                {
+                    isEven
+                }**/
+            }
+            
+            
+            
+            /**int a = 0;
         for (int i = 1; i < arrayOfIntegers.Length; i++)
             {
                 if (arrayOfIntegers[i] % i == 0){a++;}
             }
-        if(a == 0) { }
+        if(a == 0) { }**/
         }
 
-       
+
     }
 }
