d0eec6288558c2272764b78f51467075065a859b
Munirudeen Akanbi <adsdesigntech@gmail.com>
2024-07-15 21:53:27 +0100

    phone number validatorr

---
+++ PhoneNumberValidator/Model/NumberValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneNumberValidator.Model
+{
+    public static  class NumberValidator
+    {
+        public static string PhoneValidator(string phoneNumber)
+        {
+            var starter = "+234";
+            if(!phoneNumber.StartsWith(starter))
+            {
+                return "Phone number must start with +234";
+            }
+            var numberAfter = phoneNumber.Substring(4);
+            if(numberAfter.Length > 10)
+            {
+                return "Invalid: didgit after +234 must not be greater than 10 ";
+            }
+           
+            foreach(char c in numberAfter)
+            {
+                if (!char.IsDigit(c))
+                {
+                    return "Invalid format, input must be a number after +234";
+                }
+            }
+            return "Correct number format";
+        }
+    }
+}
---
+++ PhoneNumberValidator/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PhoneNumberValidator.Model;
+
+Console.WriteLine(NumberValidator.PhoneValidator("08038324425"));
+Console.WriteLine(NumberValidator.PhoneValidator("+234704867"));
+Console.WriteLine(NumberValidator.PhoneValidator("+234yegkdjjd"));
+Console.WriteLine(NumberValidator.PhoneValidator("+2347037147031"));
